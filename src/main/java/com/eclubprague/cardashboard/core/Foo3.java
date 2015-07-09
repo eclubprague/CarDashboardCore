bf5a4fb3a4b0e7c51d36ecbcbcb99cd3e329e149
Lukas Hruby <lukas.hruby@gmail.com>
2015-07-09 11:39:42 +0200

    renamed foo to fox

--- src/main/java/com/eclubprague/cardashboard/core/Foo3.java
+++
@@ -1,7 +0,0 @@
-package com.eclubprague.cardashboard.core;
-
-/**
- * Created by Lukas on 9. 7. 2015.
- */
-public class Foo3 {
-}
--- src/main/java/com/eclubprague/cardashboard/core/Fox.java
+++ src/main/java/com/eclubprague/cardashboard/core/Fox.java
@@ -3,6 +3,8 @@ package com.eclubprague.cardashboard.core;
 /**
  * Created by Lukas on 9. 7. 2015.
  */
-public class Foo {
-    public static int x = 10;
+public class Fox {
+    public int returnTen(){
+        return 10;
+    }
 }
