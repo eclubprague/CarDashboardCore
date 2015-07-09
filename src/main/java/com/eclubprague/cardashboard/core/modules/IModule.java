106036e5a0aece4fa0e9bf1ba40806a5b76ed89d
blahami2 <blahami2@gmail.com>
2015-07-09 11:38:33 +0200

    git modules - interface, icon

---
+++ src/main/java/com/eclubprague/cardashboard/core/modules/IModule.java
@@ -0,0 +1,10 @@
+package com.eclubprague.cardashboard.core.modules;
+
+/**
+ * Created by Michael on 9. 7. 2015.
+ *
+ * Interface for modules.
+ */
+public interface IModule {
+
+}
---
+++ src/main/java/com/eclubprague/cardashboard/core/modules/SimpleAbstractModule.java
@@ -0,0 +1,10 @@
+package com.eclubprague.cardashboard.core.modules;
+
+/**
+ * Created by Michael on 9. 7. 2015.
+ *
+ * Simple implementation of IModule interface.
+ */
+public class SimpleAbstractModule implements IModule {
+
+}
