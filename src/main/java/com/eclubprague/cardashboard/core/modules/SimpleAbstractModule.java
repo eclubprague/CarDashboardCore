be3d7e4e890ed5210b2c2338763794d272ef7f8b
blahami2 <blahami2@gmail.com>
2015-07-14 12:51:57 +0200

    simple implementation update

--- src/main/java/com/eclubprague/cardashboard/core/modules/SimpleAbstractModule.java
+++ src/main/java/com/eclubprague/cardashboard/core/modules/SimpleAbstractModule.java
@@ -1,20 +1,25 @@
 package com.eclubprague.cardashboard.core.modules;
 
+import com.eclubprague.cardashboard.core.modules.models.resources.ColorResource;
 import com.eclubprague.cardashboard.core.modules.models.resources.IconResource;
 import com.eclubprague.cardashboard.core.modules.models.resources.StringResource;
 
 /**
  * Created by Michael on 9. 7. 2015.
- *
+ * <p/>
  * Simple implementation of IModule interface.
  */
-public abstract class SimpleAbstractModule implements IModule {
+public class SimpleAbstractModule implements IModule {
     private final IconResource iconResource;
     private final StringResource titleResource;
+    private final ColorResource bgColorResource;
+    private final ColorResource fgColorResource;
 
-    public SimpleAbstractModule(IconResource iconResource, StringResource titleResource) {
+    public SimpleAbstractModule(IconResource iconResource, StringResource titleResource, ColorResource bgColorResource, ColorResource fgColorResource) {
         this.iconResource = iconResource;
         this.titleResource = titleResource;
+        this.bgColorResource = bgColorResource;
+        this.fgColorResource = fgColorResource;
     }
 
     @Override
@@ -25,5 +30,15 @@ public abstract class SimpleAbstractModule implements IModule {
     @Override
     public StringResource getTitle() {
         return titleResource;
+    }
+
+    @Override
+    public ColorResource getBackgroundColor() {
+        return bgColorResource;
+    }
+
+    @Override
+    public ColorResource getForegroundColor() {
+        return fgColorResource;
     }
 }
