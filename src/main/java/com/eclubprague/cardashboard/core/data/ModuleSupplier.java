4e557814c2dc5fd6089cf55f243d5e0a91356f21
blahami2 <blahami2@gmail.com>
2015-07-16 16:14:00 +0200

    new icons, it's prettier now

--- src/main/java/com/eclubprague/cardashboard/core/data/ModuleSupplier.java
+++ src/main/java/com/eclubprague/cardashboard/core/data/ModuleSupplier.java
@@ -1,8 +1,10 @@
 package com.eclubprague.cardashboard.core.data;
 
+import com.eclubprague.cardashboard.core.R;
 import com.eclubprague.cardashboard.core.modules.TestDisplayModule;
 import com.eclubprague.cardashboard.core.modules.TestSimpleModule;
 import com.eclubprague.cardashboard.core.modules.base.IModule;
+import com.eclubprague.cardashboard.core.modules.base.models.ModuleUpdateEvent;
 import com.eclubprague.cardashboard.core.modules.base.models.resources.IconResource;
 import com.eclubprague.cardashboard.core.modules.base.models.resources.StringResource;
 
@@ -17,20 +19,60 @@ import java.util.List;
 public class ModuleSupplier {
     public static List<IModule> getHomeScreenModules() {
         List<IModule> modules = new ArrayList<>();
-        String gm = "Google maps";
-        StringBuilder sb = new StringBuilder();
-        modules.add(new TestDisplayModule(
+        TestDisplayModule testDisplayModule;
+        modules.add(testDisplayModule = new TestDisplayModule(
                 null,
                 StringResource.fromString("Speed"),
-                IconResource.fromResourceId(com.eclubprague.cardashboard.core.R.drawable.ic_language_black_24dp),
+                IconResource.fromResourceId(R.drawable.ic_directions_car_black_24dp),
                 null,
                 null,
                 StringResource.fromString("kmph")
         ));
+        modules.add(testDisplayModule = new TestDisplayModule(
+                null,
+                StringResource.fromString("Settings"),
+                IconResource.fromResourceId(R.drawable.ic_settings_black_24dp),
+                null,
+                null,
+                StringResource.fromString("clicks")
+        ));
+        testDisplayModule.onEventMainThread(new ModuleUpdateEvent(Integer.toString(999)));
+        modules.add(new TestSimpleModule(
+                null,
+                StringResource.fromString("Settings"),
+                IconResource.fromResourceId(R.drawable.ic_settings_black_24dp),
+                null, null));
+        modules.add(new TestSimpleModule(
+                null,
+                StringResource.fromString("OBD"),
+                IconResource.fromResourceId(R.drawable.ic_directions_car_black_24dp),
+                null, null));
+        modules.add(new TestSimpleModule(
+                null,
+                StringResource.fromString("Voice input"),
+                IconResource.fromResourceId(R.drawable.ic_mic_black_24dp),
+                null, null));
+        modules.add(new TestSimpleModule(
+                null,
+                StringResource.fromString("Google maps"),
+                IconResource.fromResourceId(R.drawable.ic_map_black_24dp),
+                null, null));
+        modules.add(new TestSimpleModule(
+                null,
+                StringResource.fromString("SMS"),
+                IconResource.fromResourceId(R.drawable.ic_chat_black_24dp),
+                null, null));
+        modules.add(new TestSimpleModule(
+                null,
+                StringResource.fromString("Email"),
+                IconResource.fromResourceId(R.drawable.ic_email_black_24dp),
+                null, null));
+        String gm = "Google maps";
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 33; i++) {
             sb.append(gm.charAt(i % gm.length()));
             modules.add(new TestSimpleModule(null, StringResource.fromString(sb.toString()),
-                    IconResource.fromResourceId(com.eclubprague.cardashboard.core.R.drawable.ic_language_black_24dp),
+                    IconResource.fromResourceId(R.drawable.ic_settings_black_24dp),
                     null, null));
         }
         return modules;
