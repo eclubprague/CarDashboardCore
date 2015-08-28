52614205966003e30de083d2117089ff44b177e4
Lukas Hruby <lukas.hruby@gmail.com>
2015-08-28 10:38:26 +0200

    preferences menu updated

--- src/main/java/com/eclubprague/cardashboard/core/preferences/SettingsFragment.java
+++ src/main/java/com/eclubprague/cardashboard/core/preferences/SettingsFragment.java
@@ -1,14 +1,75 @@
 package com.eclubprague.cardashboard.core.preferences;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
 import com.eclubprague.cardashboard.core.R;
 
-public class SettingsFragment extends PreferenceFragment {
+import java.util.ArrayList;
+import java.util.Set;
+
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    public static final String ENABLE_BT_KEY = "enable_bluetooth_preference";
+    public static final String BLUETOOTH_LIST_KEY = "bluetooth_list_preference";
+    public static final String IMPERIAL_UNITS_KEY = "imperial_units_preference";
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        ArrayList<CharSequence> pairedDeviceStrings = new ArrayList<>();
+        ArrayList<CharSequence> vals = new ArrayList<>();
+        ListPreference listBtDevices = (ListPreference) getPreferenceScreen()
+                .findPreference(BLUETOOTH_LIST_KEY);
+
+        final BluetoothAdapter mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter == null) {
+            listBtDevices
+                    .setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
+            listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
+            Toast.makeText(this.getActivity(), "This device does not support Bluetooth.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final Activity thisActivity = this.getActivity();
+        listBtDevices.setEntries(new CharSequence[1]);
+        listBtDevices.setEntryValues(new CharSequence[1]);
+        listBtDevices.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                if (mBtAdapter == null || !mBtAdapter.isEnabled()) {
+                    Toast.makeText(thisActivity,
+                            "This device does not support Bluetooth or it is disabled.",
+                            Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+        });
+
+
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDeviceStrings.add(device.getName() + "\n" + device.getAddress());
+                vals.add(device.getAddress());
+            }
+        }
+        listBtDevices.setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
+        listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
+    }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
     }
 }
