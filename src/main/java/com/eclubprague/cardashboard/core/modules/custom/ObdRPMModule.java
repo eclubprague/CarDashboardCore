712f134071c0a48c2ce4a9fc9871c04ae426043e
Lukas Hruby <lukas.hruby@gmail.com>
2015-09-02 16:32:45 +0200

    obd module updated

--- src/main/java/com/eclubprague/cardashboard/core/modules/custom/ObdRPMModule.java
+++ src/main/java/com/eclubprague/cardashboard/core/modules/custom/ObdRPMModule.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 
 import com.eclubprague.cardashboard.core.model.eventbus.events.GlobalExtraFastUpdateEvent;
 import com.eclubprague.cardashboard.core.modules.base.AbstractTimedUpdateDisplayModule;
+import com.eclubprague.cardashboard.core.modules.base.IModuleContext;
 import com.eclubprague.cardashboard.core.modules.base.models.resources.IconResource;
 import com.eclubprague.cardashboard.core.modules.base.models.resources.StringResource;
 import com.eclubprague.cardashboard.core.obd.OBDGatewayService;
@@ -16,6 +17,13 @@ public class ObdRPMModule extends AbstractTimedUpdateDisplayModule<GlobalExtraFa
 
     public ObdRPMModule(@NonNull StringResource titleResource, @NonNull IconResource iconResource, @NonNull StringResource unitResource) {
         super(titleResource, iconResource, unitResource);
+    }
+
+
+    @Override
+    public void onPause(IModuleContext moduleContext) {
+        super.onPause(moduleContext);
+
     }
 
     @Override
